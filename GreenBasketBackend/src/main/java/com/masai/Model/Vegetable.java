b21b8071f1862c87a01e47cec9d428ebebc6bdad
thearpankumar <arpankumar1119@outlook.com>
2025-10-29 11:48:36 +0530

    fix: jakarta, javax

--- GreenBasketBackend/src/main/java/com/masai/Model/Vegetable.java
+++ GreenBasketBackend/src/main/java/com/masai/Model/Vegetable.java
@@ -1,23 +1,19 @@
 
 package com.masai.Model;
 
-import lombok.*;
-
-import javax.persistence.Entity;
-import javax.validation.constraints.Size;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.Size;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
@@ -34,17 +30,10 @@ public class Vegetable {
 
 	private String type;
 
-
 	private Integer quantity;
 
 	private Double price;
 
 	private String imageUrl;
 
-
-
-
-	
-	
-	
 }
